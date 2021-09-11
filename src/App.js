c4357bca24e1df94a66338c4428bafea95bc1f68
Daniel <danicampu56@gmail.com>
2021-09-10 21:09:17 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar/NavBar.js";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.js";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CartProvider from "./Context/CartContext.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CartProvider>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <ItemListContainer />
+            </Route>
+
+            <Route path="/category/:id">
+              <ItemListContainer />
+            </Route>
+
+            <Route path="/item/:id">
+              <ItemDetailContainer />
+            </Route>
+          </Switch>
+        </Router>
+      </CartProvider>
     </div>
   );
 }
